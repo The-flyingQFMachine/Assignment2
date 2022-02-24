cff9f11928f02c48cc62146f267894e2c7fe9b5d
The-flyingQFMachine <ibliznik@montgomerycollege.edu>
2022-02-24 14:19:35 -0500

    Rework using provided RNG class

--- src/com/learning/Main.java
+++ src/com/learning/Main.java
@@ -6,28 +6,27 @@ public class Main {
     public static void main(String[] args) {
 	   String again = "yes";
 	   int guess;
-	   Random randomNumbers = new Random();
-	   //int randNum = randomNumbers.nextInt(100);
+
 
 	   int lowGuess = 0;
 	   int highGuess = 100;
 	   int guessCounter = 0;
-	   int guessOverLimit = 0;
 	   int numOfIterations = 0;
 	   //TODO display header
 	   Scanner keyboard = new Scanner(System.in);
 	   do {
-           int randNum = randomNumbers.nextInt(100);
+	       RNG.resetCount();
+           int randNum = RNG.rand();
            System.out.println(randNum); //For debugging
-           System.out.printf("Number of iterations: %d Guesses over limit: %d\n", numOfIterations, guessOverLimit);
+           System.out.printf("Number of iterations: %d \n", numOfIterations);
            System.out.println("Enter your first guess: ");
            guess = keyboard.nextInt();
-           guessCounter++;
+
            while (0 > guess || guess > 100) {
-               guessCounter--;
+
                System.out.println("Guess between 0 and 100. Your guess is out of range. Reenter-- ");
                guess = keyboard.nextInt();
-               guessCounter++;
+
            }
            while (guess != randNum) {
                System.out.println("Number of guesses " + guessCounter);
@@ -40,15 +39,14 @@ public class Main {
                    System.out.println("Your guess is too low");
                    lowGuess = guess;
                }
-               guessCounter++;
 
                System.out.printf("Enter your next guess between %d and %d\n", lowGuess, highGuess);
+               boolean isValid;
+               do {
                guess = keyboard.nextInt();
-               while (lowGuess > guess || guess > highGuess) {
-                   guessOverLimit++;
-                   System.out.println(">>> Guess must be between lowGuess and highGuess. Try again");
-                   guess = keyboard.nextInt();
-               }
+               isValid = RNG.inputValidation(guess, lowGuess, highGuess);
+               } while (isValid == false);
+               guessCounter = RNG.getCount();
            }
 
            System.out.println("Congratulations, you guessed correctly.");
